268ff350988bf4f5d27adba2d5c64661e89a55e6
Robertimor <dybina0012@mail.ru>
2025-02-04 19:09:48 +0300

    12356

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,12 @@
 const headerMenuList = document.querySelector(".header__menu-list")
 const headerMenuItem = document.querySelectorAll(".header__menu-item")
 
+
 // Функция для переключения стиля у кнопок навигации в header
-headerMenuList.addEventListener("click", function(e) {
+const updateHeaderMenuSelectedItem = function(e) {
     const target = e.target.closest(".header__menu-item")
-    if (!target) return    // Если клик был вне контейнера с ссылкой, то игнор
-    if (target.classList.contains("header__menu-link_selected")) return   // Если выбранный элемент навигации уже был ранее выбран (есть модификатор "selected"), то игнор
+     // Если клик был вне контейнера с ссылкой, то игнор. И если выбранный элемент навигации уже был ранее выбран (есть модификатор "selected") 
+    if (!target || target.classList.contains("header__menu-link_selected")) return   
     
     // Перебираю все элементы навигации у удаляю у них класс-модификатор "selected" (если он до этого был)
     headerMenuItem.forEach(function(item) {
@@ -17,4 +18,10 @@ headerMenuList.addEventListener("click", function(e) {
     // Добавляю модификатор "selected" выбранному элементу
     target.classList.add("header__menu-item_selected")
     target.querySelector(".header__menu-link").classList.add("header__menu-link_selected")
+}
+
+// Создаю обработчик на все элементы меню в header
+headerMenuList.addEventListener("click", function(event) {
+    updateHeaderMenuSelectedItem(event)
 })
+
