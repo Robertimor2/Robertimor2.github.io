3d21a22b228adaea6be47c5e26182577e23a272c
Robertimor <dybina0012@mail.ru>
2025-02-04 19:13:41 +0300

    123

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,9 @@ const updateHeaderMenuSelectedItem = function(e) {
     // Перебираю все элементы навигации у удаляю у них класс-модификатор "selected" (если он до этого был)
     headerMenuItem.forEach(function(item) {
         item.classList.remove("header__menu-item_selected")
-        item.querySelector(".header__menu-link").classList.remove("header__menu-link_selected")
     })
     // Добавляю модификатор "selected" выбранному элементу
     target.classList.add("header__menu-item_selected")
-    target.querySelector(".header__menu-link").classList.add("header__menu-link_selected")
 }
 
 // Создаю обработчик на все элементы меню в header
