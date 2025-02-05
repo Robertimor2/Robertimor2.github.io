143d8b6f8e0114a77abc271d3235c9b3b8710c9b
Robertimor <dybina0012@mail.ru>
2025-02-06 00:34:14 +0300

    1234

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,20 @@ headerMenuList.addEventListener("click", function(event) {
     updateHeaderMenuSelectedItem(event)
 })
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const rangeInput = document.querySelector('.slider__bar-range');
+    const afterImage = document.querySelector('.slider__cat-before-img');
+    const beforeImage = document.querySelector('.slider__cat-after-img');
+    console.log(rangeInput);
+    rangeInput.addEventListener('input', () => {
+        console.log("123");
+        //value от 0 до 100
+        const value = rangeInput.value;
+        //режет справа фото толстого кота на значение ползунка, чем больше тянешь влево, тем больше показывается фото кота
+        beforeImage.style.clipPath = `inset(0 ${value}% 0 0)`;
+        //режет слева фото худого кота, 100 и значение ползунка, получается чем больше тянешь вправо, тем меньше обрезки
+        afterImage.style.clipPath = `inset(0 0 0 ${100 - value}%)`;
+    });
+});
